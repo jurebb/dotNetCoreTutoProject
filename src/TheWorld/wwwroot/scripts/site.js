95d392e8655e4ff5b1a5bdd3c16a994c10143432
Jure Baban <jbaban00@fesb.hr>
2017-02-17 01:19:23 +0100

    JS modulo compl.

--- src/TheWorld/wwwroot/scripts/site.js
+++ src/TheWorld/wwwroot/scripts/site.js
@@ -1,1 +1,22 @@
-﻿
+﻿$(document).ready(function () {
+
+    var $main = $("#main");
+    $main.on("mouseenter", function () {
+        $main.css("background-color", "#888");
+    });
+
+    $main.on("mouseleave", () => {
+        $main.css("background-color", "");
+    });
+
+    var $btnHide = $("#sidebarToggle");
+    $btnHide.on("click", function () {
+        $("#sidebar,#wrapper").toggleClass("hide-sidebar");
+        if ($("#sidebar,#wrapper").hasClass("hide-sidebar")) {
+            $(this).text("Show menu");
+        }
+        else {
+            $(this).text("Hide menu");
+        }
+    })
+});
