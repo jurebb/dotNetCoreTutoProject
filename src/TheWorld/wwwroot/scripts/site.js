9e8167ed11e8155c2611909bbfdd333e55a1662b
Jure Baban <jbaban00@fesb.hr>
2017-02-19 18:31:09 +0100

    Bootstra, nav, navbars, grid, forms, bootswatch, fontawesome

--- src/TheWorld/wwwroot/scripts/site.js
+++ src/TheWorld/wwwroot/scripts/site.js
@@ -1,22 +1,25 @@
 ﻿$(document).ready(function () {
 
-    var $main = $("#main");
+    /*var $main = $("#main");
     $main.on("mouseenter", function () {
-        $main.css("background-color", "#888");
+        $main.css("background-color", "#d9d9d9");
     });
 
     $main.on("mouseleave", () => {
         $main.css("background-color", "");
-    });
+    });*/
 
     var $btnHide = $("#sidebarToggle");
+    var $iconHide = $("#sidebarToggle i.fa");
     $btnHide.on("click", function () {
         $("#sidebar,#wrapper").toggleClass("hide-sidebar");
         if ($("#sidebar,#wrapper").hasClass("hide-sidebar")) {
-            $(this).text("Show menu");
+            $iconHide.removeClass("fa-angle-left");
+            $iconHide.addClass("fa-angle-right");
         }
         else {
-            $(this).text("Hide menu");
+            $iconHide.removeClass("fa-angle-right");
+            $iconHide.addClass("fa-angle-left");
         }
     })
 });
